71f7bf5a68c82ff87b4eaca76a3946605d4dd8cc
ak1303 <94529241+ak1303@users.noreply.github.com>
2024-02-28 03:22:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
 //your JS code here. If required.
+let circles = document.querySelectorAll('.circle');
+let lines = document.querySelectorAll('.line');
+
+let prevBtn = document.getElementById('prev');
+let nextBtn = document.getElementById('next');
+let index=0;
+prevBtn.addEventListener('click',(e)=>{
+	if(index>0){
+		circles[index].classList.remove('active');
+		lines[index-1].classList.remove('active');
+		index--;
+		updateButtonStates();
+	}
+});
+nextBtn.addEventListener('click',(e)=>{
+	if(index+1<circles.length){
+		circles[index+1].classList.add('active');
+		lines[index].classList.add('active'); 
+		index++;
+		updateButtonStates();
+	}
+});
+function updateButtonStates() {
+  prevBtn.disabled = index === 0;
+  nextBtn.disabled = index === circles.length - 1;
+}
+updateButtonStates();
+
+
+
+
